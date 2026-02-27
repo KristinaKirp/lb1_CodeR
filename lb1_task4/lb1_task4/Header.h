7b18c82c7fc04c33f2b06804e421fab80d4abe26
Kristina K <noompup@gmail.com>
2026-02-27 12:32:39 +0500

    Delete lb1_task4 directory

--- lb1_task4/lb1_task4/Header.h
+++
@@ -1,7 +0,0 @@
-#include <string>
-
-// Основная функция для запуска вычисления
-bool SolveExpression(const std::string& expression);
-
-// Рекурсивная функция вычисления без использования циклов
-bool EvaluateRecursive(const std::string& expression, int& pos);
